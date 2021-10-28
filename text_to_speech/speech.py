cd0f3549ebe7aec8c30592f6563c23db40bc3a61
Gabriel Monteiro <gmonteeeiro@gmail.com>
2021-10-28 17:35:48 -0300

    Create Speech class

--- text_to_speech/speech.py
+++ text_to_speech/speech.py
@@ -1,2 +1,15 @@
+import pyttsx3
+
 class Speech:
-    pass
+    def __init__(self: object) -> None:
+        self.__phrases = ''
+        self.__engine = pyttsx3.init()
+
+    def add_phrase(self: object, phrase: str) -> None:
+        self.__phrases += f'\r\n{phrase}'
+
+    def save(self: object, filename='speech', extension='mp3') -> None:
+        file = f'{filename}.{extension}'
+
+        self.__engine.save_to_file(self.__phrases, file)
+        self.__engine.runAndWait()
