0e970816c8a3e163dde42cd4c7540cf73906424a
Gabriel Monteiro <gmonteeeiro@gmail.com>
2021-11-03 12:21:54 -0300

    Add missed 'dot' in images extensions

--- image_manager/functions.py
+++ image_manager/functions.py
@@ -2,23 +2,24 @@ from os import path
 import requests
 import shutil
 
+
 def download_image(image_url: str, output_dir: str, name='') -> str:
     extension = image_url.split('.')[-1]
     if name == '':
         name = image_url.split("/")[-1]
     else:
         if extension not in name:
-            name = name + extension
-        
+            name = name + '.' + extension
+
     filename = path.join(output_dir, name)
 
-    r = requests.get(image_url, stream = True)
+    r = requests.get(image_url, stream=True)
     if r.status_code == 200:
         r.raw.decode_content = True
 
         with open(filename, 'wb') as f:
             shutil.copyfileobj(r.raw, f)
-        
+
         return filename
     else:
         return None
