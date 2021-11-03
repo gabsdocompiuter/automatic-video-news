6b44ea080f8735eac755701d4329af52070286e9
Gabriel Monteiro <gmonteeeiro@gmail.com>
2021-11-03 13:31:52 -0300

    Implements new VideoMaker class

--- video_maker/video_maker.py
+++ video_maker/video_maker.py
@@ -1,11 +1,27 @@
 from moviepy.editor import *
+import util
+
 
 class VideoMaker:
     def __init__(self: object) -> None:
-        pass
+        self.__clips = []
 
-    def create_clip() -> ImageClip:
-        pass
+    def create_clip(
+        self: object,
+        image_filename: str,
+        audio_filename: str,
+    ):
+        audio_clip = AudioFileClip(audio_filename)
+        clip = ImageClip(image_filename)
+
+        clip.audio = audio_clip
+        clip.duration = audio_clip.duration
+        clip.fps = 24
+
+        self.__clips.append(clip)
 
     def save_video(self: object, filename: str, extension='mp4') -> None:
-        pass
+        file = util.filename(filename, extension)
+
+        video = concatenate_videoclips(self.__clips)
+        video.write_videofile(file)
