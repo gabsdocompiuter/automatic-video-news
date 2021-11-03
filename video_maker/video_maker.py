df024ec35f9b06b72b9ab9a7f68ce5cd50d96da3
Gabriel Monteiro <gmonteeeiro@gmail.com>
2021-11-03 12:29:14 -0300

    Recreate VideoMaker class

--- video_maker/video_maker.py
+++ video_maker/video_maker.py
@@ -1,22 +1,11 @@
-import util
-import ffmpeg
+from moviepy.editor import *
 
 class VideoMaker:
-    def __init__(self: object, background=None, audio=None) -> None:
-        self.__background = background
-        self.__audio = audio
-    
-    def set_background(self: object, file: str, image=True, video=False) -> None:
-        self.__background = file
+    def __init__(self: object) -> None:
+        pass
 
-    def set_audio(self: object, file: str) -> None:
-        self.__audio = file
+    def create_clip() -> ImageClip:
+        pass
 
     def save_video(self: object, filename: str, extension='mp4') -> None:
-        file = util.filename(filename, extension)
-
-        input_still = ffmpeg.input(self.__background)
-        input_audio = ffmpeg.input(self.__audio)
-
-        out = ffmpeg.output(input_audio, input_still, file)
-        out.run(overwrite_output=True)
+        pass
